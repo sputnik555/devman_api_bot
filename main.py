55ff9f643d4e75e8a26b7ce21a87c472b3939480
Alexey Beletsky <sputnik2009@gmail.com>
2022-09-22 07:59:08 +0300

    catch errors and write to log.

--- main.py
+++ main.py
@@ -6,6 +6,18 @@ import telegram
 from environs import Env
 
 
+class MyLogsHandler(logging.Handler):
+
+    def emit(self, record):
+        log_entry = self.format(record)
+        self.bot.send_message(chat_id=self.chat_id, text=log_entry)
+
+    def __init__(self, tg_token, chat_id):
+        logging.Handler.__init__(self)
+        self.chat_id = chat_id
+        self.bot = telegram.Bot(tg_token)
+
+
 def send_message(tg_token, chat_id, is_negative, lesson_title, lesson_url):
     bot = telegram.Bot(tg_token)
     if is_negative:
@@ -18,35 +30,43 @@ def send_message(tg_token, chat_id, is_negative, lesson_title, lesson_url):
 
 
 if __name__ == '__main__':
-    logging.warning('Бот запущен')
     env = Env()
     env.read_env()
     tg_token = env.str('TELEGRAM_TOKEN')
+    tg_loger_token = env.str('TELEGRAM_LOGGER_TOKEN')
     chat_id = env('TG_CHAT_ID')
+
+    logger = logging.getLogger('bot_logger')
+    logger.setLevel(logging.INFO)
+    logger.addHandler(MyLogsHandler(tg_loger_token, chat_id))
+    logger.info('Бот запущен')
+
     headers = {
         'Authorization': env('DEVMAN_TOKEN')
     }
     params = {}
     while True:
         try:
-            response = requests.get('https://dvmn.org/api/long_polling/', headers=headers, params=params)
-            response.raise_for_status()
-        except requests.exceptions.ConnectionError:
-            time.sleep(10)
-            continue
-        except requests.exceptions.ReadTimeout:
-            continue
-        attempt_statuses = response.json()
-        if attempt_statuses.get('status') == 'found':
-            params['timestamp'] = attempt_statuses.get('last_attempt_timestamp')
-            for attempt in attempt_statuses.get('new_attempts'):
-                send_message(
-                  tg_token,
-                  chat_id,
-                  attempt.get('is_negative'),
-                  attempt.get('lesson_title'),
-                  attempt.get('lesson_url')
-                )
-        elif attempt_statuses.get('status') == 'timeout':
-            params['timestamp'] = attempt_statuses.get('timestamp_to_request')
-          
+            try:
+                response = requests.get('https://dvmn.org/api/long_polling/', headers=headers, params=params)
+                response.raise_for_status()
+            except requests.exceptions.ConnectionError:
+                time.sleep(10)
+                continue
+            except requests.exceptions.ReadTimeout:
+                continue
+            attempt_statuses = response.json()
+            if attempt_statuses.get('status') == 'found':
+                params['timestamp'] = attempt_statuses.get('last_attempt_timestamp')
+                for attempt in attempt_statuses.get('new_attempts'):
+                    send_message(
+                      tg_token,
+                      chat_id,
+                      attempt.get('is_negative'),
+                      attempt.get('lesson_title'),
+                      attempt.get('lesson_url')
+                    )
+            elif attempt_statuses.get('status') == 'timeout':
+                params['timestamp'] = attempt_statuses.get('timestamp_to_request')
+        except Exception as err:
+            logger.exception(err)
